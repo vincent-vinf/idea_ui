09160be3f16c3713e5c8fe0ad0973e39b056de69
Vincent <857618786vin@gmail.com>
2021-11-16 14:09:43 +0800

    Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,140 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomeState extends State<Home> {
+  bool clickedCentreFAB = false;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  static const List<Widget> _pages = <Widget>[
+    Text("1"),
+    Text("2"),
+    Text("3"),
+    Text("4"),
+  ];
+
+  //call this method on click of each bottom app bar item to update the screen
+  void updateTabSelection(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: FractionalOffset.center,
+            child: _pages[_selectedIndex],
+          ),
+          Align(
+            alignment: FractionalOffset.bottomCenter,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 250),
+              //if clickedCentreFAB == true, the first parameter is used. If it's false, the second.
+              height:
+                  clickedCentreFAB ? MediaQuery.of(context).size.height : 10.0,
+              width:
+                  clickedCentreFAB ? MediaQuery.of(context).size.height : 10.0,
+              decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.circular(clickedCentreFAB ? 0.0 : 300.0),
+                  color: Colors.blue),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: IconButton(
+        padding: const EdgeInsets.all(0),
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        icon: const Image(
+          image: AssetImage('assets/image/add.png'),
         ),
+        iconSize: 70,
+        onPressed: () {
+          setState(() {
+            clickedCentreFAB = !clickedCentreFAB;
+          });
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          margin: const EdgeInsets.only(left: 12.0, right: 12.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                //update the bottom app bar view each time an item is clicked
+                onPressed: () {
+                  updateTabSelection(0);
+                },
+                padding: const EdgeInsets.all(3),
+                icon: const Image(
+                  image: AssetImage('assets/image/home.png'),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  updateTabSelection(1);
+                },
+                padding: const EdgeInsets.all(3),
+                icon: const Image(
+                  image: AssetImage('assets/image/tag.png'),
+                ),
+              ),
+              const SizedBox(
+                width: 50.0,
+              ),
+              IconButton(
+                onPressed: () {
+                  updateTabSelection(2);
+                },
+                padding: const EdgeInsets.all(3),
+                icon: const Image(
+                  image: AssetImage('assets/image/chat.png'),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  updateTabSelection(3);
+                },
+                padding: const EdgeInsets.all(3),
+                icon: const Image(
+                  image: AssetImage('assets/image/user.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+        //to add a space between the FAB and BottomAppBar
+        shape: const CircularNotchedRectangle(),
+        color: Colors.white,
+      ),
     );
   }
 }
