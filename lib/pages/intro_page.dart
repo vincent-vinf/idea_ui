f3d524fecd37a3d9064908e92761b27c53168604
Vincent <857618786vin@gmail.com>
2021-12-13 19:11:49 +0800

    介绍页框架

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/colors/gf_color.dart';
+import 'package:getwidget/components/image/gf_image_overlay.dart';
+import 'package:getwidget/components/intro_screen/gf_intro_screen.dart';
+import 'package:getwidget/components/intro_screen/gf_intro_screen_bottom_navigation_bar.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  late PageController _pageController;
+  late List<Widget> slideList;
+  late int initialPage;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 2);
+    initialPage = _pageController.initialPage;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: GFIntroScreen(
+        color: Colors.blueGrey,
+        slides: slides(),
+        pageController: _pageController,
+        currentIndex: initialPage,
+        pageCount: 5,
+        introScreenBottomNavigationBar: GFIntroScreenBottomNavigationBar(
+          pageController: _pageController,
+          pageCount: slideList.length,
+          currentIndex: initialPage,
+          onForwardButtonTap: () {
+            _pageController.nextPage(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.linear);
+          },
+          onBackButtonTap: () {
+            _pageController.previousPage(
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.linear);
+          },
+          navigationBarColor: Colors.white,
+          showDivider: false,
+          activeColor: GFColors.SUCCESS,
+        ),
+      ),
+    );
+  }
+
+  List<Widget> slides() {
+    slideList = [
+      Container(
+        child: GFImageOverlay(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.all(16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 70.0, left: 20),
+                child: Text(
+                  'Welcome!',
+                  style: TextStyle(
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      fontSize: 25),
+                ),
+              ),
+            ],
+          ),
+          color: Colors.orange,
+          image: const AssetImage('assets image here'),
+          boxFit: BoxFit.cover,
+          colorFilter:
+          ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.darken),
+          borderRadius: BorderRadius.circular(5),
+        ),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(0),
+            image: DecorationImage(
+              image: AssetImage('assets image here'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2), BlendMode.darken),
+            )),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(0),
+            image: DecorationImage(
+              image: AssetImage('assets image here'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2), BlendMode.darken),
+            )),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(0),
+            image: DecorationImage(
+              image: AssetImage('assets image here'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.2), BlendMode.darken),
+            )),
+      ),
+    ];
+    return slideList;
+  }
+}
